5d6bcbcbe5a461aef1e7687c3f02fe6d87c46648
Anrian Sihotang <anrishotang91@gmail.com>
2022-06-03 16:39:13 +0700

    belajar bootstrap group

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Media ,Alert,Badge,Button,Breadcrumb} from "react-bootstrap";
+import { Container, Row, Col, Media ,Alert,
+	Badge,Button,Breadcrumb,ButtonGroup, ButtonToolbar} from "react-bootstrap";
 class App extends Component {
 	render() {
 		return ( 
@@ -65,6 +66,18 @@ class App extends Component {
 				</Col>
 				<Col>
 					<Button variant="info">Kirim</Button>
+					<br/>
+					<ButtonToolbar aria-label="Toolbar with button groups">
+						<ButtonGroup className="mr-2" aria-label="First group">
+							<Button>1</Button> <Button>2</Button> <Button>3</Button> <Button>4</Button>
+						</ButtonGroup>
+						<ButtonGroup className="mr-2" aria-label="Second group">
+							<Button>5</Button> <Button>6</Button> <Button>7</Button>
+						</ButtonGroup>
+						<ButtonGroup aria-label="Third group">
+							<Button>8</Button>
+						</ButtonGroup>
+					</ButtonToolbar>
 				</Col>
 			</Row>
 			</Container>

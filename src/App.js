dc20ff4dad3ef3c9e49ee787bf82b41f3e1040b8
Anrian Sihotang <anrishotang91@gmail.com>
2022-06-03 12:39:44 +0700

    Belajar Bootstrap Layout dan Media

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
+import { Container, Row, Col, Media } from "react-bootstrap";
 class App extends Component {
 	render() {
 		return ( 
-			<div>
-			<Button variant="primary">Testing</Button>
-			<h1>hello bootsrap</h1>
-			</div>
+			<Container>
+			<Row>
+				<Col>
+				<Media>
+					<img
+						width={64}
+						height={64}
+						className="mr-3"
+						src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png"
+						alt="Gambar Sate"
+					/>
+					<Media.Body>
+						<h4>Makanan Sate</h4>
+						<p>
+						Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+						ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+						tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
+						Donec lacinia congue felis in faucibus.
+						</p>
+					</Media.Body>
+				</Media>
+				</Col>
+				<Col>
+				<Media>
+					<img
+						width={64}
+						height={64}
+						className="mr-3"
+						src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png"
+						alt="Gambar Sate"
+					/>
+					<Media.Body>
+						<h4>Makanan Sate</h4>
+						<p>
+						Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+						ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+						tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
+						Donec lacinia congue felis in faucibus.
+						</p>
+					</Media.Body>
+				</Media>
+				</Col>
+			</Row>
+			<Row>
+				<Col>1 of 3</Col>
+				<Col>2 of 3</Col>
+				<Col>3 of 3</Col>
+			</Row>
+			</Container>
 		);
 	}
 }
